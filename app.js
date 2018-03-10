1f8d0a5cf6b975dba31ba26a3dd86d85dfa24767
kolton-s <k_simpson22@fanshaweonline.ca>
2018-03-10 01:15:17 -0500

    added more movies

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 app.use('/getCars', require('./routes/getCars'));
 app.use('/api', require('./routes/api'));
-app.use('/movies', require('./routes/movies.js'));
+app.use('/movies', require('./routes/movies'));
 
 
 // catch 404 and forward to error handler
